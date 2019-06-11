e199e7583db46beb31ab95925739e530329884fe
Tianyu Zhou <3140102448@zju.edu.cn>
2019-06-11 21:23:35 +0800

    21：33

--- leetcode-CPP/leetcode-CPP/源.cpp
+++ leetcode-CPP/leetcode-CPP/源.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
+class Solution {
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		vector<vector<int>> res;
+		sort(nums.begin(), nums.end());
+		vector<int> tmp;
+		for (int i = 0; i < nums.size() - 2; i++) {
+			if (i > 0 && nums[i] == nums[i - 1])
+				continue;
+			tmp.push_back(nums[i]);
+			int left = i + 1, right = nums.size() - 1;
+			while (left > i && right > left) {
+				if (nums[i] + nums[left] + nums[right] == 0) {
+					tmp.push_back(nums[left]);
+					tmp.push_back(nums[right]);
+					res.push_back(tmp);
+					tmp.pop_back();
+					tmp.pop_back();
+					left++;
+					right--;
+				}
+				else if (nums[i] + nums[left] + nums[right] > 0)
+					right--;
+				else
+					left++;
+			}
+			tmp.pop_back();
+		}
+		return res;
+	}
+};
 int main() {
-	cout << "hello world!" << endl;
+	vector<int> v;
+	int t;
+	cin >> t;
+	while (getchar() != '\n') {
+		v.push_back(t);
+		cin >> t;
+	}
+	v.push_back(t);
+	Solution s;
+	vector<vector<int>> res = s.threeSum(v);
+	system("pause");
+	return 0;
 }
